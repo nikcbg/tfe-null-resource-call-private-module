5a255f68028f6d5aaaf58f9c1e5c100563497a69
nikcbg <43785382+nikcbg@users.noreply.github.com>
2019-03-28 13:46:57 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,6 @@
+//--------------------------------------------------------------------
+// Modules
+module "resource_for_private_module" {
+  source  = "app.terraform.io/tforg123/resource-for-private-module/null"
+  version = "0.1"
+}
